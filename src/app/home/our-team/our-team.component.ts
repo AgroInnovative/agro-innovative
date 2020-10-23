f603f69d29671cf02a00f9e9f6019aedafdeb9f2
Mihindu Ranasinghe <mihindu.21523@gmail.com>
2020-10-23 13:06:09 +0530

    team tab updated

--- src/app/home/our-team/our-team.component.ts
+++ src/app/home/our-team/our-team.component.ts
@@ -10,26 +10,31 @@ export class OurTeamComponent {
     {
       name: 'Lilan Udayanga',
       position: 'Doctor of Finance',
+      description:'Researcing on - The impact of new technologies on the sustainability development of Agribusinesses in Sri Lanka',
       avatar: 'assets/img/team/lilan.png',
     },
     {
       name: 'Mihindu Ranasinghe',
-      position: 'Director of Software Development',
+      position: 'Director of Technology & Innovations',
+      description:'Researching on - Machine Learning solutions on predicting pest attacks in paddy cultivation',
       avatar: 'assets/img/team/mihindu.png',
     },
     {
       name: 'Pankajan Satkunam',
       position: 'Director of Research and Development',
+      description:'Researching on - converting air-condition machines into a weather data gathering weather station',
       avatar: 'assets/img/team/Pankajan.png',
     },
     {
       name: 'Zeena Youhan',
       position: 'Director of Public Relations',
+      description:'Researching on - Reviewing pros and cons of existing solutions on predicting pest attacks in paddy cultivation',
       avatar: 'assets/img/team/zeena.png',
     },
     {
       name: 'Sanjula Karunarathne',
       position: 'Director of Marketing',
+      description:'Researching on - Machine Learning solutions on predicting pest attacks in paddy cultivation',
       avatar: 'assets/img/team/sanjula.png',
     },
   ];
