3b517497ffdb2c6ec163ed1b80fa82014234a6d8
Mihindu Ranasinghe <mihindu.21523@gmail.com>
2020-12-08 13:51:26 +0530

    Redesigned for Ericsson

--- src/app/home/our-team/our-team.component.ts
+++ src/app/home/our-team/our-team.component.ts
@@ -7,35 +7,36 @@ import { Component } from '@angular/core';
 })
 export class OurTeamComponent {
   teamMembers = [
-    {
-      name: 'Lilan Udayanga',
-      position: 'Director of Finance',
-      description:'Currently researcing on - The impact of new technologies on the sustainability development of Agribusinesses in Sri Lanka',
-      avatar: 'assets/img/team/lilan.png',
-    },
+    
     {
       name: 'Mihindu Ranasinghe',
-      position: 'Director of Technology & Innovations',
+      // position: 'Technology & Innovations',
       description:'Currently researching on - Machine Learning and AI solutions on predicting upcoming pest attacks in agriculture sector',
       avatar: 'assets/img/team/mihindu.png',
     },
     {
       name: 'Pankajan Satkunam',
-      position: 'Director of Research and Development',
+      // position: 'Research and Development',
       description:'Currently researching on - Converting air-condition machines into weather data gathering weather stations',
       avatar: 'assets/img/team/Pankajan.png',
     },
     {
       name: 'Zeena Youhan',
-      position: 'Director of Public Relations',
+      // position: 'Public Relations',
       description:'Currently researching on - Reviewing existing solutions for predicting upcoming pest attacks in agriculture',
       avatar: 'assets/img/team/zeena.png',
     },
     {
       name: 'Sanjula Karunarathne',
-      position: 'Director of Marketing',
+      // position: 'Marketing',
       description:'Researching on - Machine Learning solutions on predicting pest attacks in paddy cultivation',
       avatar: 'assets/img/team/sanjula.png',
+    },
+    {
+      name: 'Lilan Udayanga',
+      // position: 'Finance',
+      description:'Currently researcing on - The impact of new technologies on the sustainability development of Agribusinesses in Sri Lanka',
+      avatar: 'assets/img/team/lilan.png',
     },
   ];
 }
